bee16e51fc2e374b8feddf36c8e2a3197d452081
evanasse <etienne.vanasse@outlook.com>
2022-09-10 22:24:00 -0400

    fix user-exists.sh

--- scripts/bash-utils/user-exists.sh
+++ scripts/bash-utils/user-exists.sh
@@ -3,8 +3,11 @@
 source "$(dirname "${BASH_SOURCE[0]}")/echoes.sh"
 
 user_exists() {
-    if ! id "$1" &>/dev/null; exit_code=$?; then
-        echo_code $exit_code "User '$1' does not exist." >&2
+    if id "$1" &>/dev/null; then
+        exit_code=$?
+    else
+        exit_code=$?
+        echo_code $exit_code "User '$1' does not exist."
     fi
 
     return $exit_code
--- scripts/create-user.sh
+++ scripts/create-user.sh
@@ -5,26 +5,29 @@ source "$(dirname "${BASH_SOURCE[0]}")/bash-utils/user-exists.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/bash-utils/read-username.sh"
 
 if [[ -z $1 ]]; then
-    username=$(read_username)
+    read_username
 else
     username=$1
 fi
 
 if user_exists $username; then
-    echo_code $? "User '$username' already exists. Nothing to do."
+    exit_code=$?
+    echo_code $exit_code "User '$username' already exists. Nothing to do."
 else
+    exit_code=$?
     echo_code $? "Creating new user..."
 
     useradd -m -G users,wheel $username
     passwd $username
 
     # Roll back user creation
-    if ! $?; exit_code=$?; then
+    exit_code=$?
+    if [[ $exit_code -ne 0 ]]; then
         echo_code $exit_code "Error during user creation. Rolling back changes."
         userdel -r $username
 
         exit $exit_code
     fi
 
-    echo_code $? "User '$username' created."
+    echo_code $exit_code "User '$username' created."
 fi
