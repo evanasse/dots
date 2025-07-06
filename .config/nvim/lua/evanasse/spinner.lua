1b69227d4981ac70433e376fa61524649a0c5a79
evanasse <etienne.vanasse@outlook.com>
2025-07-05 20:31:11 -0400

    nvim: formatting spinner.lua

--- .config/nvim/lua/evanasse/spinner.lua
+++ .config/nvim/lua/evanasse/spinner.lua
@@ -1,8 +1,8 @@
-function reverse(t)
+local function reverse(t)
   local n = #t
   local i = 1
   while i < n do
-    t[i],t[n] = t[n],t[i]
+    t[i], t[n] = t[n], t[i]
     i = i + 1
     n = n - 1
   end
@@ -11,27 +11,28 @@ end
 
 local M = {}
 
-M.full_spinner = function ()
-  return {"", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""}
+M.full_spinner = function()
+  return { "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+    "", "", "", "" }
 end
 
-M.full_spinner_rev = function ()
+M.full_spinner_rev = function()
   return reverse(M.full_spinner())
 end
 
-M.half_spinner = function ()
-  return {"", "", "", "", "", "", "", "", "", "", "", "", ""}
+M.half_spinner = function()
+  return { "", "", "", "", "", "", "", "", "", "", "", "", "" }
 end
 
-M.half_spinner_rev = function ()
+M.half_spinner_rev = function()
   return reverse(M.half_spinner())
 end
 
-M.quarter_spinner = function ()
-  return {"", "", "", "", "", "", ""}
+M.quarter_spinner = function()
+  return { "", "", "", "", "", "", "" }
 end
 
-M.quarter_spinner_rev = function ()
+M.quarter_spinner_rev = function()
   return reverse(M.quarter_spinner())
 end
 
